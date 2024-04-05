b64640ef46851fafb3d96d75915ac0afea7987d2
Hiroko0218 <148058244+Hiroko0218@users.noreply.github.com>
2024-04-05 13:30:40 +0800

    Delete jsd2304-se directory

--- jsd2304-se/src/main/java/reflect/demo/Singleton.java
+++
@@ -1,9 +0,0 @@
-package reflect.demo;
-
-public class Singleton {
-    private static Singleton instance = new Singleton();
-    private Singleton(){}
-    public static Singleton getInstance(){
-        return instance;
-    }
-}
--- jsd2304-se/src/main/java/string/LengthDemo.java
+++
@@ -1,9 +0,0 @@
-package string;
-
-public class LengthDemo {
-    public static void main(String[] args) {
-        String str = "我愛java";
-        int len = str.length();
-        System.out.println(len);//6
-    }
-}
